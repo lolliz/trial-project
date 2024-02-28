0d59d7c1b13faec6f77af26ac348b48e3175c69d
lolliz <shinginaelizaveta@mail.ru>
2024-02-28 20:01:12 +0300

    add files

---
+++ main.py
@@ -0,0 +1,8 @@
+# print("Result: ", 5, sep="", end="!\n")
+# print("Second \" \t \\ line")
+# print("Result:", 5 // 3)
+print("Result:", min(3, 5, 0, -3))
+print("Result:", abs(-5))
+# print("Result:", round(5 / 3))
+
+input("Введите возраст:")
